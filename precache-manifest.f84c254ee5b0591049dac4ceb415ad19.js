11974b366cd99561e031f910876c467cdc860298
Taylor Thompson <ttaythompson@gmail.com>
2019-06-24 09:15:17 -0400

    Updates

---
+++ precache-manifest.f84c254ee5b0591049dac4ceb415ad19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1011d5b73c4073c0729ead6e91a549",
+    "url": "/stackathon-project/index.html"
+  },
+  {
+    "revision": "b5e2efc671c22f6d6019",
+    "url": "/stackathon-project/static/css/main.9ff6a35f.chunk.css"
+  },
+  {
+    "revision": "92c949bfdc1b376ee3ff",
+    "url": "/stackathon-project/static/js/2.f920ca29.chunk.js"
+  },
+  {
+    "revision": "b5e2efc671c22f6d6019",
+    "url": "/stackathon-project/static/js/main.811a6adc.chunk.js"
+  },
+  {
+    "revision": "dd76c2ea922754cf9da7",
+    "url": "/stackathon-project/static/js/runtime~main.5a49075d.js"
+  }
+]);
